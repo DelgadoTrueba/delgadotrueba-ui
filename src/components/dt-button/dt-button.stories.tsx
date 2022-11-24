c376dd75a5c69ed5106aae3110457c21e2d2e9b1
Delgado Trueba <delgadotrueba@outlook.com>
2022-11-24 21:03:30 +0000

    chore/tarea-1_add_storybook_webpack5_webcomponents: create dt-button

---
+++ src/components/dt-button/dt-button.stories.tsx
@@ -0,0 +1,36 @@
+// import readme from './readme.stories.mdx';
+
+export default {
+    title: 'Demo/MyButton',
+    argTypes: {
+      label: { type: 'text', description: 'The text which is shown as label' },
+      onClick: { action: 'onClick' },
+    },
+  //   parameters: {
+  //     docs: {
+  //       page: readme,
+  //     },
+  // },
+};
+
+
+const Template = (args: any) => {
+
+  const handleClick = ($event: any) => {
+    console.log("handleClick")
+    console.log($event)
+    args.onClick()
+  }
+
+  // const MyButton = document.createElement('dt-button');
+  // MyButton.label = args.label;
+  // MyButton.addEventListener('onDtClick', handleClick );
+  // return MyButton;
+
+  return <dt-button label={args.label} onDtClick={(ev: any) => handleClick(ev)}/>;
+}
+
+export const Default = Template.bind({}) as any;
+Default.args = {
+    label: "Example"
+};
