84d1cd11c57d5f2829751537f72e7ef6563528b0
Delgado Trueba <delgadotrueba@outlook.com>
2022-11-27 15:57:38 +0000

    chore/tarea-2_stencil_docs_storybook: create custom-elements.json ii
    HOW:
    * npx cem analyze --stencil --globs 'src/**/*.tsx' --exclude 'src/**/*stories.tsx'

--- stencil.config.ts
+++ stencil.config.ts
@@ -4,10 +4,6 @@ export const config: Config = {
   namespace: 'delgadotrueba-ui',
   outputTargets: [
     {
-      type: 'docs-vscode',
-      file: 'custom-elements.json'
-    },
-    {
       type: 'dist',
       esmLoaderPath: '../loader',
     },
