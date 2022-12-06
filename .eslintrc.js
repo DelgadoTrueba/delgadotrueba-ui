5965ee3dca53bc670b5c443e8f4781df70581ff9
Delgado Trueba <delgadotrueba@outlook.com>
2022-12-06 09:06:35 +0000

    chore(tarea-4): :hammer: add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: 'standard-with-typescript',
+  extends: [
+    'standard-with-typescript',
+    "plugin:prettier/recommended"
+  ],
   overrides: [
   ],
   parserOptions: {
