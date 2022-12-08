020e27dc7154bcb3b2e9ef109a75da6d061a389c
Delgado Trueba <delgadotrueba@outlook.com>
2022-12-08 09:04:29 +0000

    chore(tarea-4): :wrench: lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'standard-with-typescript',
+    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
     "plugin:@stencil/recommended"
   ],
@@ -23,9 +23,11 @@ module.exports = {
     'loader',
     'www',
     '**/*.d.ts',
-    "stencil.config.ts"
+    "stencil.config.ts",
+    ".*"
   ],
   rules: {
+    "@typescript-eslint/no-explicit-any": "warn",
     '@typescript-eslint/strict-boolean-expressions': ['error', { allowString: true }],
     "react/jsx-no-bind": [ "error", { "ignoreDOMComponents": true } ],
     '@stencil/strict-boolean-conditions': "off",
