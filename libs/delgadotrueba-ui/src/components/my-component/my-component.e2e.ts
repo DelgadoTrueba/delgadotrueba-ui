5cb5c153f48dd9df29e1e3571deaedead5e6346b
Delgado Trueba <delgadotrueba@outlook.com>
2022-12-11 18:47:28 +0000

    chore(tarea-1): :wrench: package-based delgadotrueba-ui

--- libs/delgadotrueba-ui/src/components/my-component/my-component.e2e.ts
+++ libs/delgadotrueba-ui/src/components/my-component/my-component.e2e.ts
@@ -30,3 +30,41 @@ describe('my-component', () => {
     expect(element.textContent).toEqual(`Hello, World! I'm James Earl Quincy`);
   });
 });
+
+
+import { newSpecPage } from '@stencil/core/testing';
+import { MyComponent } from './my-component';
+
+describe('my-component-2', () => {
+  it('renders', async () => {
+    const { root } = await newSpecPage({
+      components: [MyComponent],
+      html: '<my-component></my-component>',
+    });
+    expect(root).toEqualHtml(`
+      <my-component>
+        <mock:shadow-root>
+          <div>
+            Hello, World! I'm
+          </div>
+        </mock:shadow-root>
+      </my-component>
+    `);
+  });
+
+  it('renders with values', async () => {
+    const { root } = await newSpecPage({
+      components: [MyComponent],
+      html: `<my-component first="Stencil" last="'Don't call me a framework' JS"></my-component>`,
+    });
+    expect(root).toEqualHtml(`
+      <my-component first="Stencil" last="'Don't call me a framework' JS">
+        <mock:shadow-root>
+          <div>
+            Hello, World! I'm Stencil 'Don't call me a framework' JS
+          </div>
+        </mock:shadow-root>
+      </my-component>
+    `);
+  });
+});
--- libs/delgadotrueba-ui/stencil.config.ts
+++ libs/delgadotrueba-ui/stencil.config.ts
@@ -1,11 +1,28 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'delgadotrueba-ui',
   taskQueue: 'async',
   sourceMap: true,
-
+  
   extras: {
     experimentalImportInjection: true,
   },
+  
+  namespace: 'delgadotrueba-ui',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
 };
